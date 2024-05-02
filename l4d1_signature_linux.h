546f47155ae92705fd6cf078f3301ea8bbd84289
Accelerator <accelerator74@gmail.com>
2024-05-02 15:30:07 +0300

    Update l4d1_signature_linux.h

--- l4d1_signature_linux.h
+++ l4d1_signature_linux.h
@@ -7,19 +7,19 @@ const char* matchmaking_dll[] = {"matchmaking_ds.so", "matchmaking.so", 0};
 
 //CBaseServer::GetMaxHumanPlayers(void)const
 //function is in engine.so
-const char* friends_lobby = "\x16\x56\x53\x83\xEC\xC3\xE8\xC3\xC3\xC3\xC3\x81\xC3\xC3\xC3\xC3\xC3\x8B\x74\xC3\xC3\x8B\x83";
+const char* friends_lobby = "\x10\x56\x53\x83\xEC\x6C\xE8\xC3\xC3\xC3\xC3\x81\xC3\xDA\xA4\x23\x00";
 unsigned char friends_lobby_new[] = {0x06, 0x00, 0xB8, 0x3C, 0x00, 0x00, 0x00, 0xC3};
 void* friends_lobby_org = NULL;
 
 //CBaseServer::ConnectClient(netadr_s &, int, int, int, char  const*, char  const*, char  const*, int, CUtlVector<CLC_SplitPlayerConnect, CUtlMemory<CLC_SplitPlayerConnect, int>> &, bool)
 //function is in engine.so, loc jz/jnz, above #Valve_Reject_Hidden_Game
-const char* lobby_sux = "\x0A\x8B\x86\x78\x01\x00\x00\x85\xC0\x0F\x85";
+const char* lobby_sux = "\x0A\x8B\xAE\x78\x01\x00\x00\x85\xED\x0F\x85";
 const char* lobby_sux_new = "\x06\x00\xB8\x01\x00\x00\x00\x90";
 void* lobby_sux_org = NULL;
 
 //CBaseServer::ConnectClient(netadr_s &, int, int, int, char  const*, char  const*, char  const*, int, CUtlVector<CLC_SplitPlayerConnect, CUtlMemory<CLC_SplitPlayerConnect, int>> &, bool)
 //function is in engine.so, above #Valve_Reject_Server_Full
-const char* max_players = "\x0F\xFF\x50\xC3\x29\xC7\x01\xEF\x3B\xC3\xC3\xC3\x00\x00\x0F\x8E";
+const char* max_players = "\x0F\xFF\x50\xC3\x29\xC7\x01\xFD\x3B\xC3\xC3\xC3\x00\x00\x0F\x8E";
 unsigned char max_players_new[]= {0x06, 0x07, 0x83, 0xFF, 0x3C, 0x90, 0x90, 0x90};
 void* max_players_org = NULL;
 
@@ -31,7 +31,7 @@ void* server_bplayers_org = NULL;
 
 //CTerrorGameRules::ClientConnected(edict_t *, char  const*, char  const*, char *, int)
 //function is in server.so
-const char* human_limit = "\x12\x8B\x54\xC3\xC3\x8B\x02\x89\x14\xC3\xFF\x90\xC3\x02\x00\x00\x39\xF8\x7E";
+const char* human_limit = "\x12\x8B\x00\xC3\xC3\x24\x40\x89\x14\xC3\xFF\x90\xC3\x02\x00\x00\x39\xF0\x7E";
 const char* human_limit_new = "\x02\x11\x90\x90";
 void* human_limit_org = NULL;
 
@@ -43,7 +43,7 @@ void* players_org = NULL;
 
 //CBaseServer::SetReservationCookie(unsigned long long, char  const*, ...)
 //function in engine.so
-const char* unreserved = "\x1F\x55\x57\x56\x53\x81\xEC\xC3\xC3\x00\x00\xE8\xC3\xC3\xC3\xC3\x81\xC3\xC3\xC3\xC3\xC3\x8B\xB4\x24\x60\x01\x00\x00\x8B\xAC\x24";
+const char* unreserved = "\x11\x55\x57\x56\x53\x81\xEC\x3C\x01\x00\x00\xE8\xC3\xC3\xC3\xC3\x81\xC3\x33\x8E\x23\x00";
 const char* unreserved_new = "\x01\x00\xC3";
 void* unreserved_org = NULL;
 
